0649103f63079cdc4414e0dcba13ab2a45e9d55d
Rahul Vaidya <github@rvaidya.com>
2026-08-25 12:15:06 -0700

    Temporarily apply exact CI clang-format output

--- .ci/static_analysis_check.sh
+++ .ci/static_analysis_check.sh
@@ -1,12 +1,24 @@
 #!/bin/sh -eux
 
 clang-format --version
+
+git checkout -B formatter-work origin/fix/postgres-bingo-regression-tests-lint
+
 clang-format -i \
   bingo/postgres/src/pg_am/pg_bingo_build.cpp \
   bingo/postgres/src/pg_am/pg_bingo_update.cpp
 
-git diff -- \
+if ! git diff --quiet -- \
   bingo/postgres/src/pg_am/pg_bingo_build.cpp \
   bingo/postgres/src/pg_am/pg_bingo_update.cpp
+do
+  git config user.name "Bingo clang-format diagnostic"
+  git config user.email "noreply@github.com"
+  git add \
+    bingo/postgres/src/pg_am/pg_bingo_build.cpp \
+    bingo/postgres/src/pg_am/pg_bingo_update.cpp
+  git commit -m "Apply clang-format 11.0.1 to Bingo PostgreSQL changes"
+  git push origin HEAD:fix/postgres-bingo-regression-tests-lint
+fi
 
 exit 1

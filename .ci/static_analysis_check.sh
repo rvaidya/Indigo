752816fd89eaf97aad3bea0f9138b62289d49c99
Rahul Vaidya <github@rvaidya.com>
2026-08-25 12:11:32 -0700

    Temporarily capture clang-format output

--- .ci/static_analysis_check.sh
+++ .ci/static_analysis_check.sh
@@ -1,36 +1,12 @@
 #!/bin/sh -eux
 
-# C++
-# TODO
-#mkdir build-clang-tidy
-#cd build-clang-tidy
-#cmake .. -DUSE_CLANG_TIDY=ON
-#cd ..
-#clang-tidy --version
-#find . -not -path "./third_party/*" -a \( -iname "*.h" -o -iname "*.hpp" -o -iname "*.cpp" -o -iname "*.c" \) -exec clang-tidy -p build-clang-tidy --header-filter="*third-party*" {} +
-
 clang-format --version
-find . -not -path "./third_party/*" -a -not -path "./*build*/*" -a \( -iname "*.h" -o -iname "*.hpp" -o -iname "*.cpp" -o -iname "*.c" \) -exec clang-format -Werror --dry-run {} +
+clang-format -i \
+  bingo/postgres/src/pg_am/pg_bingo_build.cpp \
+  bingo/postgres/src/pg_am/pg_bingo_update.cpp
 
-# Python
+git diff -- \
+  bingo/postgres/src/pg_am/pg_bingo_build.cpp \
+  bingo/postgres/src/pg_am/pg_bingo_update.cpp
 
-isort --version
-black --version
-pflake8 --version
-for folder in api/http api/python bingo/bingo-elastic/python api/tests/integration utils/indigo-service/backend/service
-do
-  cd ${folder}
-  isort --check .
-  black --check .
-  pflake8 .
-  cd -
-done
-
-mypy --version
-for folder in api/http api/python bingo/bingo-elastic/python utils/indigo-service/backend/service
-do
-    cd ${folder}
-    export PYTHONPATH=${PWD}
-    mypy --exclude=build .
-    cd -
-done
+exit 1
